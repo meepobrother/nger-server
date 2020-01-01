231073fd51b8d59de4448b13b29b8310b42006fc
imeepos <admin@meepo.com.cn>
2020-01-01 23:58:30 +0800

    upgrade nger core

--- packages/core/dist/@nger/server/lib/index.js
+++ packages/core/dist/@nger/server/lib/index.js
@@ -12,8 +12,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tokens_1 = require("./tokens");
 const core_1 = require("@nger/core");
 const http_1 = require("http");
-const http_2 = require("@nger/http");
-const operators_1 = require("rxjs/operators");
 let ServerModule = class ServerModule {
 };
 ServerModule = __decorate([
@@ -27,94 +25,6 @@ ServerModule = __decorate([
                         if (listener)
                             listener(req, res);
                     });
-                },
-                deps: [core_1.Injector]
-            },
-            {
-                provide: tokens_1.SERVER_LISTENER,
-                useFactory: (injector) => (req, res) => {
-                    let data = Buffer.from(``);
-                    let isHandler = false;
-                    req.on('close', () => {
-                        if (isHandler)
-                            return;
-                        isHandler = true;
-                        sendRequest(data);
-                    });
-                    req.on('data', (chunk) => {
-                        data = Buffer.concat([data, chunk]);
-                    });
-                    req.on('end', () => {
-                        if (isHandler)
-                            return;
-                        isHandler = true;
-                        sendRequest(data);
-                    });
-                    req.on('error', (err) => {
-                        res.statusCode = 500;
-                        res.statusMessage = err.message;
-                        res.end();
-                    });
-                    function sendRequest(data) {
-                        const dataStr = data.toString('utf8');
-                        let dataObj;
-                        try {
-                            dataObj = JSON.parse(dataStr);
-                        }
-                        catch (e) {
-                            dataObj = dataStr;
-                        }
-                        const request = new http_2.HttpRequest(req.method, req.url, dataObj, {
-                            headers: new http_2.HttpHeaders(req.headers)
-                        });
-                        const client = injector.get(http_2.HttpClient);
-                        res.setTimeout(60 * 60 * 30);
-                        let hasSend = false;
-                        function sendData(data) {
-                            res.write(data);
-                        }
-                        client.request(request).pipe(operators_1.takeLast(1)).subscribe(response => {
-                            if (response instanceof http_2.HttpResponse) {
-                                if (!hasSend) {
-                                    response.headers.forEach((key, val) => {
-                                        res.setHeader(key, val);
-                                    });
-                                }
-                                hasSend = true;
-                                res.statusCode = 200;
-                                if (typeof response.body === 'object') {
-                                    const data = Buffer.from(JSON.stringify(response.body));
-                                    sendData(data);
-                                }
-                                else if (typeof response.body === 'string' ||
-                                    typeof response.body === 'number' ||
-                                    typeof response.body === 'boolean' ||
-                                    typeof response.body === 'bigint') {
-                                    const data = Buffer.from(`${response.body}`);
-                                    sendData(data);
-                                }
-                                else if (Buffer.isBuffer(response.body)) {
-                                    const data = response.body;
-                                    sendData(data);
-                                }
-                                else if (Array.isArray(response.body)) {
-                                    const data = Buffer.from(JSON.stringify(response.body));
-                                    sendData(data);
-                                }
-                                else {
-                                    throw new Error(`can not support ${typeof response.body}`);
-                                }
-                            }
-                        }, (err) => {
-                            res.statusCode = err.status || err.code || 500;
-                            res.statusMessage = `SERVER ERROR`;
-                            res.end(err.stack);
-                        }, () => {
-                            res.statusCode = 200;
-                            res.statusMessage = 'ok';
-                            res.end();
-                        });
-                    }
                 },
                 deps: [core_1.Injector]
             }
--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,8 +1,6 @@
 import { SERVER, SERVER_LISTENER } from "./tokens";
 import { Module, Injector } from "@nger/core";
-import { createServer, IncomingMessage, ServerResponse } from "http";
-import { HttpClient, HttpRequest, HttpResponse, HttpHeaders } from '@nger/http'
-import { takeLast } from 'rxjs/operators'
+import { createServer } from "http";
 @Module({
   providers: [
     {
@@ -12,91 +10,6 @@ import { takeLast } from 'rxjs/operators'
         return createServer((req, res) => {
           if (listener) listener(req, res)
         });
-      },
-      deps: [Injector]
-    },
-    {
-      provide: SERVER_LISTENER,
-      useFactory: (injector: Injector) => (req: IncomingMessage, res: ServerResponse) => {
-        let data: Buffer = Buffer.from(``)
-        let isHandler: boolean = false;
-        req.on('close', () => {
-          if (isHandler) return;
-          isHandler = true;
-          sendRequest(data)
-        })
-        req.on('data', (chunk: Buffer) => {
-          data = Buffer.concat([data, chunk])
-        })
-        req.on('end', () => {
-          if (isHandler) return;
-          isHandler = true;
-          sendRequest(data)
-        })
-        req.on('error', (err: Error) => {
-          res.statusCode = 500;
-          res.statusMessage = err.message
-          res.end();
-        })
-        function sendRequest(data: Buffer) {
-          const dataStr = data.toString('utf8')
-          let dataObj: any;
-          try {
-            dataObj = JSON.parse(dataStr)
-          } catch (e) {
-            dataObj = dataStr;
-          }
-          const request = new HttpRequest(req.method as any, req.url!, dataObj, {
-            headers: new HttpHeaders(req.headers as any)
-          })
-          const client = injector.get(HttpClient)
-          res.setTimeout(60 * 60 * 30)
-          let hasSend = false;
-          function sendData(data: Buffer) {
-            res.write(data)
-          }
-          client.request(request).pipe(
-            takeLast(1)
-          ).subscribe(response => {
-            if (response instanceof HttpResponse) {
-              if (!hasSend) {
-                response.headers.forEach((key, val) => {
-                  res.setHeader(key, val)
-                })
-              }
-              hasSend = true;
-              res.statusCode = 200;
-              if (typeof response.body === 'object') {
-                const data = Buffer.from(JSON.stringify(response.body))
-                sendData(data)
-              } else if (
-                typeof response.body === 'string' ||
-                typeof response.body === 'number' ||
-                typeof response.body === 'boolean' ||
-                typeof response.body === 'bigint'
-              ) {
-                const data = Buffer.from(`${response.body}`)
-                sendData(data)
-              } else if (Buffer.isBuffer(response.body)) {
-                const data = response.body;
-                sendData(data)
-              } else if (Array.isArray(response.body)) {
-                const data = Buffer.from(JSON.stringify(response.body))
-                sendData(data)
-              } else {
-                throw new Error(`can not support ${typeof response.body}`)
-              }
-            }
-          }, (err: any) => {
-            res.statusCode = err.status || err.code || 500;
-            res.statusMessage = `SERVER ERROR`;
-            res.end(err.stack);
-          }, () => {
-            res.statusCode = 200;
-            res.statusMessage = 'ok';
-            res.end();
-          })
-        }
       },
       deps: [Injector]
     }
